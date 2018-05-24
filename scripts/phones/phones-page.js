e85790f7bca6dfb9bbe30184d5cb7cfde4770a31
Evgeniy Gusev <egusev@megatec.ru>
2018-05-24 17:29:49 +0500

    sort added

--- scripts/phones/phones-page.js
+++ scripts/phones/phones-page.js
@@ -7,16 +7,32 @@ export default class PhonesPage {
   constructor({ element }) {
     this._element = element;
 
+
     this._catalogue = new PhonesCatalogue({
       element: this._element.querySelector('[data-component="phones-catalog"]'),
       phones: PhonesService.getPhones(),
     });
 
-    // this._catalogue._element.addEventListener('phoneSelected', (event) => {
+    this._sortSelect = this._element.querySelector('[data-component="phones-sort"]');
+    this._sortSelect.onchange = _sortChanged.bind(this);
+    function _sortChanged(){
+        let sortVal = this._sortSelect.value;
+        this._catalogue.Sort(sortVal);
+    };
+
+    //так и не понял, почему в этом варианте обработчик вообще не вызывается
+    /*this._sortSelect.addEventListener('onchange', (function () {
+        let sortVal = this._sortSelect.value;
+        this._catalogue.Sort(sortVal);
+    }).bind(this));*/
+
+
+      // this._catalogue._element.addEventListener('phoneSelected', (event) => {
     this._catalogue.on('phoneSelected', (event) => {
       let phoneId = event.detail;
-
       console.log(phoneId);
     });
+
+
   }
 }
