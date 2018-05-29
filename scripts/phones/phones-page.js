5facf61747968b222c21568e976468ec505c5474
Berdnik Elena <e.berdnik@amio.ru>
2018-05-29 11:37:46 +0300

    filter was moved

--- scripts/phones/phones-page.js
+++ scripts/phones/phones-page.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import PhonesService from './services/phones-service.js';
+import Filter from './services/filter-service.js';
 import PhonesCatalogue from './components/phones-catalog.js';
 import PhonesSorting from './components/phones-sorting.js';
 import PhonesSearch from './components/phones-search.js';
@@ -9,21 +10,18 @@ export default class PhonesPage {
   constructor({ element }) {
     this._element = element;
 
+    this._sorting = new PhonesSorting({
+      element: this._element.querySelector('[data-component="phones-sorting"]')
+    });
+
+    this._search = new PhonesSearch({
+      element: this._element.querySelector('[data-component="phones-search"]')
+    });
+
     PhonesService.then(phones => {
       this._catalogue = new PhonesCatalogue({
         element: this._element.querySelector('[data-component="phones-catalog"]'),
-        phones
-      });
-
-      // this._catalogue._element.addEventListener('phoneSelected', (event) => {
-      this._catalogue.on('phoneSelected', (event) => {
-        let phoneId = event.detail;
-
-        console.log(phoneId);
-      });
-
-      this._sorting = new PhonesSorting({
-        element: this._element.querySelector('[data-component="phones-sorting"]')
+        phones: phones
       });
 
       this._sorting.on('phoneSorting', (event) => {
@@ -32,15 +30,15 @@ export default class PhonesPage {
           ._render();
       });
 
-      this._search = new PhonesSearch({
-        element: this._element.querySelector('[data-component="phones-search"]')
-      });
-
       this._search.on('phoneSearch', (event) => {
-        this._catalogue
-          ._search(event.detail);
-      });
+        let isEmpty = Filter({
+            query: event.detail, 
+            phones: phones, 
+            element: this._element.querySelector('[data-component="phones-catalog"]')
+        });
 
+        this._element.querySelector('[data-search-info]').classList.toggle('hidden', isEmpty);
+      });
     });
 
   }
---
+++ scripts/phones/services/filter-service.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const Filter = ({query, phones, element}) => {
+  for (const phone of phones) {
+    element.querySelector(`[data-phone-id="${phone.id}"]`)
+      .classList.toggle('hidden', !phone.name.toUpperCase().includes(query.toUpperCase()));
+  }
+
+  return element.querySelectorAll('.hidden').length <  Object.keys(phones).length;
+};
+
+export default Filter;
