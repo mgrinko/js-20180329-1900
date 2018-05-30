d29248ba0dfdbe83f0ff6c4d674bb2f685a6115d
Slooo <borisworking@gmail.com>
2018-05-30 21:43:08 +0500

    search & sorting
    Created a search and sorting

--- scripts/phones/phones-page.js
+++ scripts/phones/phones-page.js
@@ -2,14 +2,21 @@
 
 import PhonesService from './services/phones-service.js';
 import PhonesCatalogue from './components/phones-catalog.js';
+import Sorting from './components/sorting.js';
+import Searching from './components/searching.js';
 
 export default class PhonesPage {
   constructor({ element }) {
     this._element = element;
 
-    this._catalogue = new PhonesCatalogue({
-      element: this._element.querySelector('[data-component="phones-catalog"]'),
-      phones: PhonesService.getPhones(),
+    this._renderCatalogue(PhonesService.getPhones());
+
+    this._sorting = new Sorting({
+      element: this._element.querySelector('[data-component="sorting"]')
+    });
+
+    this._searching = new Searching({
+      element: this._element.querySelector('[data-component="searching"]')
     });
 
     // this._catalogue._element.addEventListener('phoneSelected', (event) => {
@@ -17,6 +24,52 @@ export default class PhonesPage {
       let phoneId = event.detail;
 
       console.log(phoneId);
+    });
+
+    this._sorting.on('sortSelected', (event) => {
+      let sortBy = event.detail;
+
+        let phones = PhonesService.getPhones().sort(function(a, b) {
+          if (a[sortBy] > b[sortBy]) {
+              return 1;
+            }
+            if (a[sortBy] < b[sortBy]) {
+              return -1;
+            }
+
+            return 0;
+        });
+
+        this._renderCatalogue(phones);
+
+    });
+
+    this._searching.on('searchInput', (event) => {
+      let search = event.detail.toLowerCase();
+
+      let phones = PhonesService.getPhones();
+      let searchPhones = [];
+
+      setTimeout(() => {
+        for(let phone in phones) {  
+          let lowName = phones[phone].name.toLowerCase();
+
+          if(lowName.indexOf(search) !== -1) {
+            searchPhones.push(phones[phone]);
+          }
+        }
+
+        this._renderCatalogue(searchPhones);
+      }, 1000);
+
+    });
+
+  }
+
+  _renderCatalogue(phones) {
+    this._catalogue = new PhonesCatalogue({
+      element: this._element.querySelector('[data-component="phones-catalog"]'),
+      phones: phones
     });
   }
 }
