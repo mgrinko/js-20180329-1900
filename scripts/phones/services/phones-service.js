8f7d9ce80869b7d2804810aa8b86e214508b8e82
nchuraeva <nchuraeva123@gmail.com>
2018-06-10 15:17:40 +0300

    promise was added

--- scripts/phones/services/phones-service.js
+++ scripts/phones/services/phones-service.js
@@ -3,30 +3,73 @@
 const PhonesService = {
 
     loadPhones(filter, callback) {
-        this._sendRequest('/api/phones', (phones) => {
+        let promise = this._sendRequest('/api/phones');
+        promise.then((phones) => {
             const filteredPhones = this._search(phones,filter.query);
             const sortedPhones = this._sorting(filteredPhones, filter.sort);
 
             callback(sortedPhones);
         });
+
+        setTimeout(() => {
+            promise.then((phones) => console.log(phones));
+        }, 2000);
     },
 
 
     loadPhone(phoneId, callback) {
-        this._sendRequest(`/api/phones/${ phoneId }`, callback);
+       const promise = this._sendRequest(`/api/phones/${ phoneId }`);
+       promise.then(callback);
+
     },
 
-    _sendRequest(url, callback, { method = 'GET' } = {}) {
+    _sendRequest(url) {
+        let promise = {
+            _successCallbacks: [],
+            _errorCallbacks:[],
+            _status:'pending',
+            _result: null,
+
+            then(callback) {
+                if(this._status === 'fulfilled') {
+                    callback(this._result);
+                } else {
+                    this._successCallbacks.push(callback);
+                }
+            },
+            catch() {
+                this._errorCallbacks.push(callback);
+            },
+            resolve(data) {
+                this._status = 'fulfilled';
+                this._result = data;
+                this._successCallbacks.forEach((callback) => {
+                    callback(data);
+                })
+            },
+            reject(error) {
+                this._status = 'rejected';
+                this._result = error;
+                this._errorCallbacks.forEach((callback) => {
+                    callback(error);
+                })
+            }
+        };
         let xhr = new XMLHttpRequest();
 
-        xhr.open(method, url, true);
+        xhr.open('GET', url, true);
 
         xhr.send();
 
         xhr.onload = () => {
             let data = JSON.parse(xhr.responseText);
-            callback(data);
+            promise.resolve(data);
         };
+
+        xhr.onerror = () => {
+            promise.reject(xhr.status + xhr.statusText);
+        }
+        return promise;
     },
 
     _sorting(phones, sort) {
